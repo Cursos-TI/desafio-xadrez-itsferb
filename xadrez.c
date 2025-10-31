de9aebf9a1abc815656e4a07013908026992a942
It's Fer <fernandabelokurows@gmail.com>
2025-10-31 02:19:25 +0000

    COMMIT - Desafio: nível novato

--- xadrez.c
+++ xadrez.c
@@ -1,21 +1,88 @@
 #include <stdio.h>
 
 // Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    /*********** Variáveis ***********/
+    // Declaração de variáveis de peças e casas
+    int peca; 
+    int casas;
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    /*********** Escolha da Peça ***********/
+    //Apresenta para o usuário a opção para selecionar qual peça jogar
+    do {
+        printf("PEÇAS DE XADREZ:\n");
+        printf("1-Torre\n");
+        printf("2-Bispo\n");
+        printf("3-Rainha\n");
+        printf("Informe a peça a ser movimentada: ");
+        scanf("%d", &peca);
+    } while (peca < 1 || peca > 3);
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    /*********** Movimentação da Torre ***********/
+    // Move-se em linha reta horizontalmente ou verticalmente. Deve movimentar-se cinco casas para a direita.
+    //1-Torre
+
+    //Apresenta para o usuário a opção para selecionar quantas casas jogar
+    if (peca == 1) {
+            printf("Peça escolhida: 1-Torre\n");
+            do {
+                printf("Número de casas a serem movimentadas para a direita (até 5): ");
+                scanf("%d", &casas);
+            } while (casas < 1 || casas > 5);
+            printf("A Torre andou %d casas para a direita.\n", casas);
+
+    // Movimento com Do - While
+        int i = 0;
+            do {
+                printf("Casa %d: Direita\n", i + 1);
+                i++;
+        } while (i < casas);
+    }
+
+
+    /*********** Movimentação do Bispo ***********/
+    // Move-se na diagonal. Deve movimentar-se cinco casas na diagonal para cima e à direita.
+    //2-Bispo
+
+    //Apresenta para o usuário a opção para selecionar quantas casas jogar
+
+    else if (peca == 2) {
+            printf("\nPeça escolhida: 2-Bispo\n");
+            do {
+                printf("Número de casas a serem movimentadas para cima e à direita (até 5): ");
+                scanf("%d", &casas);
+            } while (casas < 1 || casas > 5);
+            printf("O Bispo andou %d casas na diagonal.\n\n", casas);
+
+    // Movimento com WHILE
+        int i = 0;
+        while (i < casas) {
+            printf("Casa %d: Diagonal\n", i + 1);
+            i++;
+        }
+    }
+
+    /*********** Movimentação da Rainha ***********/
+    // Move-se em todas as direções. Deve movimentar-se oito casas para a esquerda.
+    //3-Rainha
+
+    //Apresenta para o usuário a opção para selecionar quantas casas jogar
+
+    else {
+            printf("\nPeça escolhida: 3-Rainha\n");
+            do {
+            printf("Número de casas a serem movimentadas para a esquerda (até 8): ");
+            scanf("%d", &casas);
+            } while (casas < 1 || casas > 8);
+            printf("A Rainha andou %d casas para a esquerda.\n\n", casas);
+
+    // Movimento com FOR
+        for (int i = 1; i <= casas; i++) {
+            printf("Casa %d: Esquerda\n", i);
+        }
+    }
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
